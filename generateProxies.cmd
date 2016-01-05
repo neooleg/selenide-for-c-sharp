3d217bdf4603efeee300921aa463b8dbe8371490
Oleg Volodin <oleg.volodin.it@gmail.com>
2016-01-05 17:15:55 +0200

    + As mentioned in the previous commit artifacts directory defined as cleanable, so it's considered in the generateProxies script and within .gitignore also.

--- generateProxies.cmd
+++ generateProxies.cmd
@@ -1,8 +1,16 @@
+:: --- Init --- ::
 @echo off
 
 set j4n_verstion=8.9
+md artifacts
 
+:: --- Selenide --- ::
+xcopy /E .\src\* .\artifacts
+
+:: --- jni4net --- ::
 copy .\bin-%j4n_verstion%\*.* artifacts
 :: jni4net jar lib was not bult therefore 8.8 uses as 8.9
 copy .\bin-8.8\jni4net.j-0.8.8.0.jar artifacts\jni4net.j-0.8.9.0.jar
+
+
 artifacts\proxygen.exe artifacts\selenide-3.0.jar -wd artifacts
