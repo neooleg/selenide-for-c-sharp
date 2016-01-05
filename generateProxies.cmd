538380cb6783ba1951b5c7af71912435f8ecbd5c
Oleg Volodin <oleg.volodin.it@gmail.com>
2016-01-05 17:01:40 +0200

    + Script for generating proxies was implemented, but doesn't work yet because of dependencies absent (not configured).

--- generateProxies.cmd
+++ generateProxies.cmd
@@ -1,1 +1,8 @@
-.\bin-8.9\proxygen.exe artifacts\selenide-3.0.jar -wd artifacts
+@echo off
+
+set j4n_verstion=8.9
+
+copy .\bin-%j4n_verstion%\*.* artifacts
+:: jni4net jar lib was not bult therefore 8.8 uses as 8.9
+copy .\bin-8.8\jni4net.j-0.8.8.0.jar artifacts\jni4net.j-0.8.9.0.jar
+artifacts\proxygen.exe artifacts\selenide-3.0.jar -wd artifacts
