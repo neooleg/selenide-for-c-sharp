ce0f784c688bf28a9b52d08c4d48a1b6ecc89e76
Oleg Volodin <oleg.volodin.it@gmail.com>
2016-02-28 14:32:38 +0200

    + Code from Selenium Camp conference with more demonstration meaning was added.

--- test/TestSelenideForNet/Program.cs
+++ test/TestSelenideForNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using com.codeborne.selenide;
+﻿using System.Windows.Forms;
+using com.codeborne.selenide;
 using net.sf.jni4net;
 using static com.codeborne.selenide.Selenide;
 using static com.codeborne.selenide.Condition;
@@ -6,28 +7,34 @@ using static com.codeborne.selenide.CollectionCondition;
 
 namespace TestSelenideForNet
 {
-    class Program
+class Program
+{
+  static void Main(string[] args)
+  {
+    ConfigureJni4NetSelenide();
+
+    open("http://google.com/ncr");
+    S("[name=q]").val("selenide").pressEnter();
+    SS("#ires .g").shouldHave(size(10));
+    S("#ires .g").shouldBe(visible).shouldHave(
+        text("Selenide: concise UI tests in Java"),
+        text("selenide.org"));
+
+    ShowSurprise();
+  }
+
+  private static void ConfigureJni4NetSelenide()
     {
-        static void Main(string[] args)
-        {
-            ConfigureJni4NetSelenide();
+        var bridgeSetup = new BridgeSetup();
+        bridgeSetup.Verbose = true;
+        bridgeSetup.AddAllJarsClassPath(".");
+        Bridge.CreateJVM(bridgeSetup);
 
-            open("http://google.com.ua/?hl=en");
-            S("[name=q]").val("selenide").pressEnter();
-            SS("#ires .g").shouldHave(size(10));
-            S("#ires .g").shouldBe(visible).shouldHave(
-                text("Selenide: concise UI tests in Java"),
-                text("selenide.org"));
-        }
-
-        private static void ConfigureJni4NetSelenide()
-        {
-            var bridgeSetup = new BridgeSetup();
-            bridgeSetup.Verbose = true;
-            bridgeSetup.AddAllJarsClassPath(".");
-            Bridge.CreateJVM(bridgeSetup);
-
-            Bridge.RegisterAssembly(typeof(Selenide).Assembly);
-        }
+        Bridge.RegisterAssembly(typeof(Selenide).Assembly);
     }
+  private static void ShowSurprise()
+    {
+        MessageBox.Show("No shit happents - test is green!", "Wow, wow wow!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+}
 }
