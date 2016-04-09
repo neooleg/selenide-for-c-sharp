c66a1df44e2f41fb37eb369940d33c5f7c5f91e2
Dima Dubyk <ddb@scalepoint.com>
2016-04-10 01:33:16 +0300

    Deleted proxies from src/proxies. Moved selenide-3.0.proxies.clr.csproj to src/proxies. Modified build.cmd and generateProxies.cmd to build selenide-3.0.proxies.clr.csproj automatically

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
+set MSBuildVersion=v4.0.30319
+
 :: CLR proxies library ::
-MSBuild .\src\proxies\clr\selenide-3.0.proxies.clr\selenide-3.0.proxies.clr.sln /property:Configuration=Debug /property:Platform="Any CPU"
+C:\WINDOWS\Microsoft.NET\Framework\%MSBuildVersion%\MSBuild .\artifacts\proxies\clr\selenide-3.0.proxies.clr.csproj
 
 :: Demo Test app ::
-MSBuild .\test\TestSelenideForNet\TestSelenideForNet.sln /property:Configuration=Debug /property:Platform="x64"
+C:\WINDOWS\Microsoft.NET\Framework\%MSBuildVersion%\MSBuild .\test\TestSelenideForNet\TestSelenideForNet.sln /property:Configuration=Debug /property:Platform="x64"
