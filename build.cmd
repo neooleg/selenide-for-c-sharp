5512da7319e3010e8e1196ab0fb18217a1bc7db3
Dima Dubyk <ddb@scalepoint.com>
2016-04-10 02:08:29 +0300

    Moved build.cmd code to generateProxies.cmd

--- build.cmd
+++
@@ -1,7 +0,0 @@
-set MSBuildVersion=v4.0.30319
-
-:: CLR proxies library ::
-C:\WINDOWS\Microsoft.NET\Framework\%MSBuildVersion%\MSBuild .\artifacts\proxies\clr\selenide-3.0.proxies.clr.csproj
-
-:: Demo Test app ::
-C:\WINDOWS\Microsoft.NET\Framework\%MSBuildVersion%\MSBuild .\test\TestSelenideForNet\TestSelenideForNet.sln /property:Configuration=Debug /property:Platform="x64"
