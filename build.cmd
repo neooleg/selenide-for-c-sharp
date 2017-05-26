1840d065ff8c961c060e5828168941f505ffcb8c
Andrey.Tretjakov <tretjakov.a@skbkontur.ru>
2017-05-26 16:50:29 +0500

    Add path white spaces support

--- build.cmd
+++ build.cmd
@@ -12,18 +12,18 @@ md artifacts
 md artifacts\proxies\clr
 
 :: --- Selenide --- ::
-cd %RootDirectory%src\bin\
-for /r %%f in (*.*) do copy %%f "%RootDirectory%artifacts"
-cd %RootDirectory%
+cd "%RootDirectory%src\bin\"
+for /r %%f in (*.*) do copy "%%f" "%RootDirectory%artifacts"
+cd "%RootDirectory%"
 
-cd %RootDirectory%artifacts
+cd "%RootDirectory%artifacts"
 set proxygenParameters=
 for %%f in (*.jar) do ( if /I "%%f" neq "selenide-4.4.3.jar" ( if "%%f" neq "selenide-4.4.3.proxies.clr.jar" (set proxygenParameters=!proxygenParameters!.\artifacts\%%f;) else (echo %%f)))
 set proxygenParameters=%proxygenParameters:~0,-1%
-cd %RootDirectory%
+cd "%RootDirectory%"
 
 :: --- jni4net --- ::
-xcopy /E /Y %RootDirectory%bin-%j4n_version%\*.* artifacts
+xcopy /E /Y "%RootDirectory%bin-%j4n_version%\*.*" artifacts
 
 :: --- Go! --- ::
 artifacts\proxygen.exe artifacts\selenide-4.4.3.jar -wd artifacts\proxies -cp "%proxygenParameters%"
@@ -34,7 +34,7 @@ xcopy selenide-4.4.3.j4n.dll ..
 xcopy selenide-4.4.3.j4n.jar ..
 
 :: Demo Test app ::
-"C:\Program Files (x86)\MSBuild\%MSBuildVersion%\Bin\MSBuild" %RootDirectory%test\TestSelenideForNet\TestSelenideForNet.csproj /t:Rebuild /property:Configuration=Debug /property:Platform="x64" 
+"C:\Program Files (x86)\MSBuild\%MSBuildVersion%\Bin\MSBuild" "%RootDirectory%test\TestSelenideForNet\TestSelenideForNet.csproj" /t:Rebuild /property:Configuration=Debug /property:Platform="x64" 
 
 cd ..
 TestSelenideForNet
